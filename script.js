b353ca2f4d5077defcc1c24ac52a20f58fe20070
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2023-05-20 11:57:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getFormvalue() {
-    //Write your code here
-
+	const fname = document.getElementById("fname").value;
+	const lname = document.getElementById("lname").value;
+    alert(`${fname} ${lname}`);
 }
